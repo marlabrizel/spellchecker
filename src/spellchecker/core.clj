b644e70b2861faee2957addc7037cb1c6d09094c
Marla Brizel <marlabrizel@gmail.com>
2016-07-08 17:07:54 -0600

    pull in word list

--- src/spellchecker/core.clj
+++ src/spellchecker/core.clj
@@ -1,5 +1,9 @@
 (ns spellchecker.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(def words
+  (set (map str/trim (str/split-lines (slurp "resources/wordsEn.txt")))))
 
 (defn -main
   "I don't do a whole lot ... yet."
