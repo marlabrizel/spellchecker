971553cd594cf234f0fee6e2c0ae77e7938fb113
Marla Brizel <marlabrizel@gmail.com>
2016-07-25 11:58:20 -0400

    the most basic of spellchecking

--- src/spellchecker/core.clj
+++ src/spellchecker/core.clj
@@ -5,6 +5,11 @@
 (def words
   (set (map str/trim (str/split-lines (slurp "resources/wordsEn.txt")))))
 
+(defn correct?
+  "Takes a word and returns true if the word is present in a collection of words"
+  [word]
+  (contains? words word))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
