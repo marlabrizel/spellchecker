4fff2410414313c8509eb17eaf90cf6776ffda2b
Marla Brizel <marlabrizel@gmail.com>
2016-08-15 11:47:23 -0600

    implement distance and main distance fns

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/marlabrizel/spellchecker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.commons/commons-lang3 "3.3.2"]]
   :main ^:skip-aot spellchecker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/spellchecker/core.clj
+++ src/spellchecker/core.clj
@@ -1,5 +1,6 @@
 (ns spellchecker.core
   (:require [clojure.string :as str])
+  (:import (org.apache.commons.lang3 StringUtils))
   (:gen-class))
 
 (def words
@@ -10,9 +11,20 @@
   [word]
   (contains? words word))
 
+(defn distance
+  "Takes two words and returns the Levenshtein distance between them."
+  [word1 word2]
+  (StringUtils/getLevenshteinDistance word1 word2))
+
+(defn min-distance
+  [word]
+  (min-key (partial distance word) "spelling" "spilling"))
+
+
 (defn -main
   [& args]
   (let [word (first args)]
     (if (correct? word)
       (println "Correct")
       (println "Did you mean xy?"))))
+
